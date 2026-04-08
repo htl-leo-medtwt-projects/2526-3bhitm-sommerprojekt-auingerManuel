22106d0436725069564dc1ff6f95b1107ba23e29
auingerManuel <m.auinger@students.htl-leonding.ac.at>
2026-04-08 08:24:17 +0200

    Grundaufbau + Icons für Startseite

---
+++ project/main/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MangaCourt</title>
+    <link rel="stylesheet" href="mainstyle.css">
+</head>
+<body>
+
+    <div id="nav">
+        
+    </div>
+    
+</body>
+</html>
